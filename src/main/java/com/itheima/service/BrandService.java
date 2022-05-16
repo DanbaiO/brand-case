6dee714936c7bc0ecf5b92eb1144f6bf88281774
Danbai <1550525269@qq.com>
2022-05-16 09:26:59 +0800

    初始化

---
+++ src/main/java/com/itheima/service/BrandService.java
@@ -0,0 +1,42 @@
+package com.itheima.service;
+
+import com.itheima.pojo.Brand;
+import com.itheima.pojo.PageBean;
+
+import java.util.List;
+
+public interface BrandService {
+
+    /**
+     * 查询所有
+     */
+    List<Brand> selectAll();
+
+    /**
+     * 添加数据
+     */
+    void add(Brand brand);
+
+
+    /**
+     * 批量删除
+     */
+    void deleteByIds( int[] ids);
+
+    /**
+     * 分页查询
+     * @param currentPage  当前页码
+     * @param pageSize   每页展示条数
+     */
+    PageBean<Brand>  selectByPage(int currentPage,int pageSize);
+
+    /**
+     * 分页条件查询
+     * @param currentPage
+     * @param pageSize
+     * @param brand
+     * @return
+     */
+    PageBean<Brand>  selectByPageAndCondition(int currentPage,int pageSize,Brand brand);
+
+}
